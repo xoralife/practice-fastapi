bd8c7b307418da87c33c34f5e7987c6630c4e4df
xoralife <abidhussainiouu@gmail.com>
2026-06-21 11:16:19 +0500

    v.3

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message for": "Hello, FastAPI!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    return {"item_id": item_id} 
+    return {"item_id": item_id}
